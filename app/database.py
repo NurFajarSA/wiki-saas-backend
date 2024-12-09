fde2cd8bddf1b54cdfb04b697ea8b62a4456fb84
Nur Fajar Sayyidul Ayyam <nur.fajarsa@gmail.com>
2024-12-09 17:34:47 +0000

    Initial commit

---
+++ app/database.py
@@ -0,0 +1,15 @@
+# app/database.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")  # e.g., postgres://user:password@localhost/wikisaas_db
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
