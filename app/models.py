05c8ca06c349b9482e963058d5a9f1d34129b2d6
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-12-10 15:48:51 +0700

    feat: add org_id for integration with auth0

--- app/models.py
+++ app/models.py
@@ -6,5 +6,6 @@ class WikiInstance(Base):
     __tablename__ = "wiki_instances"
 
     id = Column(Integer, primary_key=True, index=True)
+    org_id = Column(String, unique=True, index=True, nullable=False)
     name = Column(String, unique=True, index=True, nullable=False)
     url = Column(String, unique=True, index=True, nullable=False)
