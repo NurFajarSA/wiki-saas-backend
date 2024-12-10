f00d51bc18386eddca803660b524748cea884240
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-12-10 14:47:47 +0700

    feat: add ssl certificate

--- fastapi.sh
+++ fastapi.sh
@@ -1,2 +1,4 @@
 # fastapi.sh
-/home/nur_fajarsa/wikisaas_backend/venv/bin/uvicorn app.main:app --host 0.0.0.0 --port 8000
+/home/nur_fajarsa/wikisaas_backend/venv/bin/uvicorn app.main:app --host 0.0.0.0 --port 8000 \
+  --ssl-keyfile /etc/letsencrypt/live/kowan.nurfajar.tech/privkey.pem \
+  --ssl-certfile /etc/letsencrypt/live/kowan.nurfajar.tech/fullchain.pem
